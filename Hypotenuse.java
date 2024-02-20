40698d519a9947231cb0fd9308022a396244b981
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2024-02-20 09:56:24 +0530

    import java.util.*; public class java{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int a=sc.nextInt(),b=sc.nextInt();         double res=Math.sqrt(a*a + b*b);         System.out.printf("%.2f",res);
        }
    }

---
+++ Hypotenuse.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class java{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt(),b=sc.nextInt();
+        double res=Math.sqrt(a*a + b*b);
+        System.out.printf("%.2f",res);
+        
+    }
+}
